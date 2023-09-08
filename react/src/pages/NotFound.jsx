b2b8e5eb2a697c9597546b76e6614af002a97ec0
Eduardo <eeadomingues@gmail.com>
2023-09-08 16:54:07 -0300

    404 page not found

--- react/src/pages/NotFound.jsx
+++ react/src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return ( 
-        <div>
-            404 - Page not found
+        <div className="not-found">
+            <p>404 - Page not found</p>
         </div>
      );
 }
